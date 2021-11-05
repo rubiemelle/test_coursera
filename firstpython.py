3fc2835363cf2af0fc0cbaac0ca07a0623c5d635
rubiemelle <93761197+rubiemelle@users.noreply.github.com>
2021-11-05 16:32:34 +0800

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,1 @@
-print("Step 3: Provide the file name and the extension of the file. For example, firstpython.py and add the lines.")
+# Step 3: Provide the file name and the extension of the file. For example, firstpython.py and add the lines.
